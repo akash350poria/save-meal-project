3e5209636ac8d00102c0e55c82015fb85bb68e40
akash350poria <akash350poria@gmail.com>
2020-07-02 00:41:02 +0530

    Change logic for meal item

--- public/javascript/newIndex.js
+++ public/javascript/newIndex.js
@@ -4,23 +4,88 @@ const formDiv = document.getElementById("formDiv");
 let dayCounter = 1;
 
 addDayFunc = () => {
-  //formDiv.innerHTML = tableHeader;
   let tableHeader =
     "<h4>DAY " +
     dayCounter +
-    "</h4><form><table class='table table-bordered table-striped'><tr><th id='preBreakfast'>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th><th>Pre breakfast</th></tr></table></form><button id='addMeal' class='btn btn-danger'>Add Meal Option</button>";
+    "</h4><form><table id='mealPlanDay" +
+    dayCounter +
+    "' class='table table-bordered table-striped'><tr><th style='padding-right: 100px;'>Pre breakfast</th><th style='padding-right: 100px;'>Breakfast</th><th style='padding-right: 100px;'>Pre lunch</th><th style='padding-right: 100px;'>Lunch</th><th style='padding-right: 100px;'>Evening snack</th><th style='padding-right: 100px;'>Dinner</th><th style='padding-right: 100px;'>Post dinner</th><th style='padding-right: 100px;'>Pre workout</th><th style='padding-right: 100px;'>Post workout</th></tr></table></form><button id='addMealDay" +
+    dayCounter +
+    "'class='btn btn-danger'>Add Meal Option</button>";
   let div = document.createElement("div");
   div.innerHTML = tableHeader;
   formDiv.appendChild(div);
   dayCounter++;
 };
 
-addMealOptionFunc = () => {
-  let optionHeader =
-    "<tr><th><input type='text' class='form-control' placeholder='ITEM'></th><th><input type='text' class='form-control' placeholder='QUANTITY'></th></tr><tr><th><input type='text' class='form-control'></th><th><input type='text' class='form-control'></th></tr><tr><th><input type='text' class='form-control'></th><th><input type='text' class='form-control'></th></tr><tr><th><input type='text' class='form-control'></th><th><input type='text' class='form-control'></th></tr><tr><th><input type='text' class='form-control'></th><th><input type='text' class='form-control'></th></tr>";
-  let subTable = document.createElement("table");
-  subTable.innerHTML = optionHeader;
-  document.getElementById("preBreakfast").appendChild(subTable);
+addMealOptionFunc = (dayNumber) => {
+  let optionCounter = 1;
+  let meals = [
+    "prebreakfast",
+    "breakfast",
+    "prelunch",
+    "lunch",
+    "eveningsnack",
+    "dinner",
+    "postdinner",
+    "preworkout",
+    "postworkout",
+  ];
+  meals.forEach((meal) => {
+    let optionContent =
+      "<tr><thead>ITEM</thead><thead>QUANTITY</thead></tr><tr id='option1'" +
+      dayNumber +
+      "><td><input id='" +
+      meal +
+      "item1" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Item 1'></td><td><input id='" +
+      meal +
+      "quantity1" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Quantity 1'></td></tr><tr id='option2'" +
+      dayNumber +
+      "><td><input id='" +
+      meal +
+      "item2" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Item 2'></td><td><input id='" +
+      meal +
+      "quantity2" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Quantity 2'></td></tr><tr id='option3'" +
+      dayNumber +
+      "><td><input id='" +
+      meal +
+      "item3" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Item 3'></td><td><input id='" +
+      meal +
+      "quantity3" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Quantity 3'></td></tr><tr id='option4'" +
+      dayNumber +
+      "><td><input id='" +
+      meal +
+      "item4" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Item 4'></td><td><input id='" +
+      meal +
+      "quantity4" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Quantity 4'></td></tr><tr id='option5'" +
+      dayNumber +
+      "><td><input id='" +
+      meal +
+      "item5" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Item 5'></td><td><input id='" +
+      meal +
+      "quantity5" +
+      dayNumber +
+      "' type='text' class='form-control' placeholder='Quantity 5'></td></tr>";
+    $("#mealPlan" + dayNumber).append(optionContent);
+  });
 };
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -28,17 +93,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     $("#SaveAsTemplatePopUp").modal("show");
   });
 });
-// jQuery(document).ready(function ($) {
-//   $("#saveTemplate").click(function () {
-//     $("#SaveAsTemplatePopUp").modal("show");
-//   });
-// });
 
 addDayButton.addEventListener("click", addDayFunc);
 
-document.addEventListener("click", (e) => {
-  if (e.target && e.target.id == "addMeal") {
-    //add function to add new subtable inside each meal
-    addMealOptionFunc();
+document.addEventListener("click", (event) => {
+  if (event.target && event.target.id.includes("addMealDay")) {
+    let dayNum = "Day" + event.target.id.slice(-1);
+    console.log(dayNum);
+    addMealOptionFunc(dayNum);
   }
 });
